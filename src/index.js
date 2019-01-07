e7c3f3bf24316f68db926e5b72a7b33c18a31f26
Denis Novikov <den-vse@yandex.ru>
2019-01-08 00:32:17 +0300

    feature: add handlers to component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
-export default ({ step, steps }) => (
+export default ({ step, steps, nextStep, prevStep }) => (
   <div>
     <div>Step Progress</div>
     <div>
       {
-        steps.map((label, idx) => (
-          idx === step ? `[${label}]` : label
-        )).join('/')
+        steps.map((label, idx) => {
+          const isActiveStep = (idx === step);
+
+          const isNextStep = (step - idx === -1);
+          const isPrevStep = (step - idx === 1);
+
+          let handler = () => {};
+
+          if (isNextStep) handler = nextStep;
+          if (isPrevStep) handler = prevStep;
+
+          return (
+            <div onClick={handler}>
+              { isActiveStep ? `[${label}]` : label }
+            </div>
+          )
+        }).join(' - ')
       }
     </div>
   </div>
